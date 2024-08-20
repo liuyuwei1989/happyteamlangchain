50d1b2aa1f0fadfc733cdfd087570a3c7355a919
Jerry Liu <liuyuwei1989@yeah.net>
2024-08-20 22:01:06 +0800

    commit

--- llm.py
+++ llm.py
@@ -1,11 +1,24 @@
+from typing import List
+
 import langchain
 from langchain.agents import initialize_agent, AgentType
 from langchain.chains import LLMChain
 from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import (ChatPromptTemplate, SystemMessagePromptTemplate,
-                                    HumanMessagePromptTemplate, PromptTemplate, FewShotPromptTemplate)
+                                    HumanMessagePromptTemplate, PromptTemplate, FewShotPromptTemplate,
+                                    MessagesPlaceholder)
+from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAI
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.runnables import (
+    RunnableLambda,
+    ConfigurableFieldSpec,
+    RunnablePassthrough,
+)
 
 
 def test_response_from_open_ai_chat():
@@ -115,7 +128,72 @@ def test_agents():
     print(result)
 
 
+def test_chat_history():
+    history = ChatMessageHistory()
+    history.add_user_message("Who are you?")
+    history.add_ai_message("I'm GenAI assistant")
+    print(history)
+
+
+def test_conversation_chain():
+    class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+        """In memory implementation of chat message history."""
+
+        messages: List[BaseMessage] = Field(default_factory=list)
+
+        def add_messages(self, messages: List[BaseMessage]) -> None:
+            """Add a list of messages to the store"""
+            self.messages.extend(messages)
+
+        def clear(self) -> None:
+            self.messages = []
+
+    # Here we use a global variable to store the chat message history.
+    # This will make it easier to inspect it to see the underlying results.
+    store = {}
+
+    def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = InMemoryHistory()
+        return store[session_id]
+
+    history = get_by_session_id("1")
+    history.add_message(AIMessage(content="hello"))
+    print(store)  # noqa: T201
+    llm = ChatOpenAI(base_url="https://one.aiskt.com/v1")
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You're an assistant who's good at {ability}"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ])
+    chain = prompt | llm
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        # Uses the get_by_session_id function defined in the example
+        # above.
+        get_by_session_id,
+        input_messages_key="question",
+        history_messages_key="history",
+    )
+
+    print(chain_with_history.invoke(  # noqa: T201
+        {"ability": "math", "question": "What does cosine mean?"},
+        config={"configurable": {"session_id": "foo"}}
+    ))
+
+    # Uses the store defined in the example above.
+    print(store)  # noqa: T201
+
+    print(chain_with_history.invoke(  # noqa: T201
+        {"ability": "math", "question": "What's its inverse"},
+        config={"configurable": {"session_id": "foo"}}
+    ))
+
+    print(store)  # noqa: T201
+
+
 if __name__ == '__main__':
     langchain.verbose = True
-    langchain.debug = True
-    test_agents()
+    langchain.debug = False
+    test_conversation_chain()
