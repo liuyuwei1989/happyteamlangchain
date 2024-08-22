76d5edabdce07182cfd4ea1111b2ab62f56326cc
Tina <tingtingye06@163.com>
2024-08-22 22:55:45 +0800

    shopping wishlist story

--- shopping wishlist.py
+++ shopping wishlist.py
@@ -27,29 +27,33 @@ llm = ChatOpenAI(base_url="https://one.aiskt.com/v1")
 
 def create_chain(model=llm, prompt_1='', prompt_2='', context=''):
     # Create an user story
-    # context = "给应用新增date字段。并输出这个需求的总览Jira（类型为sprint或者spike，sprint为开发时间容易确定的类型，spike为开发时间不容易确定的类型）"
+    # context = "I want to be able to easily create and track my wish lists on a retail website " \
+    # "so that I can save products I'm interested in for later purchase and easily share my lists with friends and family."
     # 需求优化这个感觉可以和chain1并在一起
-    # prompt_1 = "帮我分析需求：{context}，如果太过于简单请帮我丰富它的内容"
+    # prompt_1 = "Analyze requirement：{context}，and enrich with additional context"
 
     # 完整需求
     context = "As a frequent online shopper," \
               " I want to be able to easily create and track my wish lists on a retail website " \
               "so that I can save products I'm interested in for later purchase and easily share my lists with friends and family." \
-              "Create a jira user story with . " \
-              "Jira type:story, task, spike"
-    prompt_1 = "analyze the requirement：{context}"
+              "Create a jira user story with enough detailed description" \
+              "Jira type:story, task, spike, sub-task"
+    prompt_1 = "analyze the requirement：{context},and enrich with additional context"
     prompt1 = PromptTemplate.from_template(prompt_1)
     chain = prompt1 | model | StrOutputParser()
     res = chain.invoke({"context": context})
-    print("Main user story")
+    print("User Story")
     print(res)
     print()
-    prompt_2 = ("{context}，create tasks need to be done based on the user story，"
-                "Create task with What，How，Why，Accept Criteria with：task1...task2...")
+    prompt_2 = ("{context}，creating tasks throughout the development process based the user story"
+                " involves breaking down the high-level functionality or requirement into smaller, "
+                "actionable steps that can be completed by developer and for tester."
+                "Create task with What，How，Why，Accept Criteria with：task1...task2..."
+                "Create test scope and test case for tester with: Test Case")
     prompt2 = PromptTemplate.from_template(prompt_2)
     chain2 = prompt2 | model | StrOutputParser()
     res2 = chain2.invoke({"context": res})
-    print("sub user story")
+    print("Tasks")
     print(res2)
 
 
